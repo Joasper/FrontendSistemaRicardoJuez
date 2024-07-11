39f848c90ae15a88ec3cb6afdb55bb1f468b21cb
JOASPER SCOTTAN FELIZ CUEVAS <78002056+Joasper@users.noreply.github.com>
2024-07-11 00:16:17 -0400

    server v8

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Servir los archivos estáticos desde la carpeta 'dist/sistema-ricardo-frontend/browser'
+app.use(express.static(path.join(__dirname, 'dist', 'browser')));
 
+// Ruta para servir el index.html en todas las rutas
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'sistema-ricardo-frontend', 'browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist',  'browser', 'index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Iniciar el servidor en el puerto definido por Heroku o 8080 localmente
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
