783ca55ee47fe23dd5805c52978eb95323fd7b47
JOASPER SCOTTAN FELIZ CUEVAS <78002056+Joasper@users.noreply.github.com>
2024-07-10 23:15:01 -0400

    app

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-
-
-// server.js
-
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + './dist'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/browser/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
