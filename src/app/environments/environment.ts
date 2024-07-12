b67e6b5e5c02c6b531d49b976fb5fd3629ee237e
JOASPER SCOTTAN FELIZ CUEVAS <78002056+Joasper@users.noreply.github.com>
2024-07-12 15:14:16 -0400

    responsive layout

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const enviroment = {
     production: false,
     API_URL: 'https://cejba-0866666a4bc9.herokuapp.com',
+    // API_URL: 'http://localhost:3000',
     API_URL_CONDITION: '/condition/filter'
   }
